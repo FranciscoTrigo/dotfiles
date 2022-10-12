c7967385fcb58e26417836cc139d410d5b27c03e
Francisco Trigo Mijes <code@trigomijes.com>
2022-10-12 13:03:51 -0500

    Kitty terminal Srcery color

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias superls="ls -la --color"
+alias ls="ls --color"
+alias tree="tree -C"
+
+set -gx EDITOR vim
