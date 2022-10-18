9038c12415a2710ad7c101ae6a2ef8798e5ea959
Francisco Trigo Mijes <code@trigomijes.com>
2022-10-18 16:59:09 -0500

    color ls perl

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+
+alias colorls="perl ~/scripts/old-script/colorls.pl
 alias superls="ls -la --color"
 alias ls="ls --color"
 alias tree="tree -C"
