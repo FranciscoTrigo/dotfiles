c7a1b30dd1048b21a43e5ccfabde8d130049ad0f
Francisco Trigo Mijes <code@trigomijes.com>
2022-11-15 03:47:53 -0600

    Nvim

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
 end
 
 
+
 alias colorls="perl ~/scripts/old-scripts/colorls.pl"
 alias superls="ls -la --color"
 alias ls="ls --color"
@@ -11,3 +12,7 @@ alias tree="tree -C"
 set -gx EDITOR vim
 
 starship init fish | source
+
+# Hishtory Config:
+export PATH="$PATH:/home/yamifrankc/.hishtory"
+source /home/yamifrankc/.hishtory/config.fish
